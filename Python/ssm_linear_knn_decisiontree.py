27ce2903872197fa225b5e5b27840945efae77ca
Proton Positron <protonsmily@protonmail.com>
2023-03-24 21:28:13 +0300

    Add some checks for types of input Data

--- Python/ssm_linear_knn_decisiontree.py
+++ Python/ssm_linear_knn_decisiontree.py
@@ -21,27 +21,29 @@ def SSMLinearRegression (x, y = None):
         - incline_coeff: k
         - shift_value: b
     """
-    if x.ndim <= 2:
-          if x.ndim == 2:
-              if x.shape[0] > 2 and x.shape[1] > 2:
-                  raise ValueError("number of rows or columns is more than two") 
-              elif x.shape[0] == 2: # Modify two-dimension array in two one-dimension arrays
-                  y = x[1,:]
-                  x = x[0,:]
-              elif x.shape[1] == 2:
-                  y = x[:,1]
-                  x = x[:,0]
-              sxy = 0 # Define temporary parameters
-              sx = 0
-              sy = 0
-              sxx = 0
-              n = len(x)
-              for i in range(0, n): # Construct one cicle for minimum processing
-                  sxy = sxy + x[i] * y[i]
-                  sx = sx + x[i]
-                  sy = sy + y[i]
-                  sxx = sxx + x[i] * x[i]
-              k = (n * sxy - sx * sy) / (n * sxx - sx * sx) # incline_coeff
-              b = (sy - k * sx) / n # shift_value
-              return (k, b)
-    else: raise ValueError("input array is more than two-dimensional")
+    if isinstance (x, np.ndarray): # Check data type, case like 'if type(x) == "numpy.ndarray"' doesn't work
+            if x.ndim <= 2:
+                  if x.ndim == 2:
+                      if x.shape[0] > 2 and x.shape[1] > 2:
+                          raise ValueError("Number of rows or columns is more than two") 
+                      elif x.shape[0] == 2: # Modify two-dimension array in two one-dimension arrays
+                          y = x[1,:]
+                          x = x[0,:]
+                      elif x.shape[1] == 2:
+                          y = x[:,1]
+                          x = x[:,0]
+                      sxy = 0 # Define temporary parameters
+                      sx = 0
+                      sy = 0
+                      sxx = 0
+                      n = len(x)
+                      for i in range(0, n): # Construct one cicle for minimum processing
+                          sxy = sxy + x[i] * y[i]
+                          sx = sx + x[i]
+                          sy = sy + y[i]
+                          sxx = sxx + x[i] * x[i]
+                      k = (n * sxy - sx * sy) / (n * sxx - sx * sx) # incline_coeff
+                      b = (sy - k * sx) / n # shift_value
+                      return (k, b)
+            else: raise ValueError("Input array is more than two-dimensional")
+    else: raise ValueError("Input argument data type is wrong")
